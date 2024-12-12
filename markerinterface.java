95447376f14f4677c2b184d02358dab4eb346bc3
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-12 12:27:49 +0530

    Add files via upload

---
+++ markerinterface.java
@@ -0,0 +1,18 @@
+import java.util.*;
+interface printable
+{
+
+}
+class h implements printable{
+    public void print(){
+        System.out.println("Hello student");
+    }
+}
+    class a 
+    {
+    public static void main(String args[])
+    {
+    h f=new h();
+    f.print();
+    }
+    }
