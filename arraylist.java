284c9ba21b6f184867bc0ffd6874d9efdd6a38d5
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-12 12:39:16 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class arraylist{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(897);
+        a.add(456);
+        a.add(324);
+        System.out.println(a);
+    }
+}
